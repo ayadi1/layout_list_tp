a135bfd888e0b1550c556278ecfbb15f35ce0f23
ayadi <ayadiossama44@gmail.com>
2025-05-19 19:45:44 +0100

    Initial commit
    Adds the basic project structure and initial files for the "tp_list_with_images" Android application.
    
    Key additions:
    - Gradle build files and wrapper
    - AndroidManifest.xml
    - MainActivity and its layout (activity_main.xml)
    - Layout for list items (layout_list.xml)
    - City data class (City.java)
    - Initial drawable resources (city images, launcher icons)
    - Basic resource files (colors.xml, strings.xml, themes.xml)
    - Test files (ExampleInstrumentedTest.java, ExampleUnitTest.java)
    - .gitignore files for project and app module

---
+++ app/src/main/java/com/example/tp_list_with_images/City.java
@@ -0,0 +1,15 @@
+package com.example.tp_list_with_images;
+
+public class City {
+    public String name;
+    public String description;
+    public int imageSrc;
+
+    public City(String name, String description, int imageSrc){
+        this.name = name;
+        this.description = description;
+        this.imageSrc = imageSrc;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tp_list_with_images/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.tp_list_with_images;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    // Tableau des noms des villes
+    City[] citys = new City[]{
+            new City("agadir", "agadir description", R.drawable.agadir),
+            new City("casa", "casa description", R.drawable.casa),
+            new City("eljadida", "eljadida description", R.drawable.eljadida),
+            new City("essaouira", "essaouira description", R.drawable.essaouira),
+            new City("fes", "fes description", R.drawable.fes),
+            new City("meknes", "meknes description", R.drawable.meknes),
+            new City("merrakech", "merrakech description", R.drawable.merrakech),
+            new City("oujda", "oujda description", R.drawable.oujda),
+            new City("tanger", "tanger description", R.drawable.tanger)
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        List<Map<String, Object>> data = new ArrayList<>();
+
+        for (City city : citys) {
+            Map<String, Object> map = new HashMap<>();
+
+            map.put("name", city.name);
+            map.put("description", city.description);
+            map.put("imageSrc", city.imageSrc); // Assuming imageSrc is an int (R.drawable.xxx)
+            data.add(map);
+        }
+
+        String[] from = {"name", "description", "imageSrc"};
+        int[] to = {R.id.txt, R.id.description, R.id.flag};
+
+
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.layout_list, from, to);
+
+        ListView listView = findViewById(R.id.l1);
+
+        listView.setAdapter(adapter);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
